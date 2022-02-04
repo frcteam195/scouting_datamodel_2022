3a9f37d6125105c7ebce74c369ee4a2804b79dc4
Mark Maciejewski <mark.maciejewski@gmail.com>
2022-02-04 11:00:48 -0500

    added deverloper name to table

--- AnalysisTypes-01.sql
+++ AnalysisTypes-01.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS `team195_scouting`.`AnalysisTypes` (
 	`AnalysisTypeID` INT NOT NULL,
 	`AnalysisType` VARCHAR(20) NULL,
 	`SortOrder` INT NULL,
+	`Developer` VARCHAR(50) NULL,
 	PRIMARY KEY(`AnalysisTypeID`))
 Engine = InnoDB;
