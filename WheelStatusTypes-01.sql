98899f1c1a45827f871da1ab24587cb5a43d02f7
Mark Maciejewski <mark.maciejewski@gmail.com>
2022-02-19 22:03:21 -0500

    cleaning up filenames

--- WheelStatusTypes-01.sql
+++
@@ -1,7 +0,0 @@
-/* Wheel Status Types */
-
-CREATE TABLE IF NOT EXISTS `team195_scouting`.`WheelStatusTypes` (
-	`WheelStatusTypeID` INT NOT NULL,
-	`WheelStatusType` VARCHAR(50) NULL,
-	PRIMARY KEY(`WheelStatusTypeID`))
-Engine = InnoDB;
