2759e467c2ce289ce81a120e2053e8c0092bde1e
Coolguy1782 <carmichall.braydon@gmail.com>
2022-04-02 12:45:31 -0400

    sql changes

--- SheetsL2Scouting.sql
+++ SheetsL2Scouting.sql
@@ -1,13 +1,11 @@
 CREATE TABLE IF NOT EXISTS `team195_scouting`.`SheetsL2Scouting` (
-	`TimeStamp` VARCHAR(2048) NOT NULL,
 	`Name` VARCHAR(2048) NOT NULL,
 	`MatchNo` INT NOT NULL,
 	`TeamNo` INT NULL, 
-	`HumanPlayerScore` INT NULL,
 	`OffensiveQualities` VARCHAR(2048) NULL,
-	`DefenseQualities` INT NULL,
+	`DefenseQualities` VARCHAR(2048)  NULL,
 	`LabelBot` VARCHAR(2048) NULL,
 	`GeneralThoughts` VARCHAR(2048) NULL,
-	`ScoreNo` INT NULL,
+	`HarishLove` VARCHAR(128) NULL,
 	PRIMARY KEY(`Name`))
 Engine = InnoDB;
